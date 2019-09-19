4a66cc2356dcb6bc30c8595bc580b4e6b06485c7
Allen Conway <allenc@magenic.com>
2019-09-18 23:55:11 -0400

    Code cleanup

--- src/components/@common/Nav/Nav.tsx
+++ src/components/@common/Nav/Nav.tsx
@@ -21,23 +21,9 @@ const Nav: React.FC = prop => {
  ];
 
   return (
-    <div>
-      <input id="nav-menu" className="nav-toggle" type="checkbox" />
-      <label className="nav-dropdown">
-        <span className="hamburger-menu">
-          <span className="icon-bar top-bar" />
-          <span className="icon-bar middle-bar" />
-          <span className="icon-bar bottom-bar" />
-        </span>
-      </label>
+    <div>      
       <nav className="nav">
-        <div className="small-nav">
-          <hr />
-        </div>
-        <ul className="nav-list">
-          <li className="nav-item">
-          </li>
-        </ul>
+
       </nav>
     </div>
   );
