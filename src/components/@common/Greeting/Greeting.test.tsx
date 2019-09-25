9c13c9106adc2f8c6b497ee7f7a6dfdf508d1efd
Allen Conway <allenc@magenic.com>
2019-09-25 15:55:58 +0800

    Fixed typo in unit test

--- src/components/@common/Greeting/Greeting.test.tsx
+++ src/components/@common/Greeting/Greeting.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { create } from "react-test-renderer";
 import Greeting from './Greeting';
 
-describe("Grreting component", () => {
+describe("Greeting component", () => {
     test("Matches the snapshot", () => {
       const greeting = create(<Greeting />);
       expect(greeting.toJSON()).toMatchSnapshot();
